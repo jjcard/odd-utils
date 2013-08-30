0a356e6dcbb11fbf839fd7f128fd8d300c80939d
John Hill <brashcaran@gmail.com>
2013-08-29 19:41:42 -0700

    added support for multiline 1337 speak. Enhancements.

--- OddUtils/src/blaa/blarg/tired/OddStringUtil.java
+++ OddUtils/src/blaa/blarg/tired/OddStringUtil.java
@@ -3,29 +3,43 @@ package blaa.blarg.tired;
 import java.util.HashMap;
 
 public class OddStringUtil {
-	private static final HashMap<String, String> special1337words = OddStringUtil.get1337words();
 	
-	public static String getAcronym(String toTurn){
-		return getAcronym(toTurn, true);
+	private static final CaseInsensitiveMap special1337words = getSpecial1337Map();
+	private static final String[] acronymToSkip = {"the", "a", "an"};
+	private static final String EMPTY_STRING = "";
+	private static final char PERIOD = '.';
+	
+	private static final char[] replace1337Chars = {'e', 'E', 'l', 'o', 'O'};
+	private static final char[] replace1337with  = {'3', '3', '1', '0', '0'};
+	
+	
+	/**
+	 * Same as calling getAcronym(phrase, true)
+	 * @param toTurn
+	 * @return
+	 * @see {@link getAcronym(String, boolean)}
+	 */
+	public static String getAcronym(String phrase){
+		return getAcronym(phrase, true);
 	}
 	/**
 	 * turns given phrase into Acronym with First letter of each word, unless the word should be skipped as in the case of 'the', 'a', etc or if the word is a number, in case it is added
 	 * to the Acronym whole. ex. Big F'ing Gun 9000 becomes BFG9000 (if usePeriods is set to false).
+	 * Should only be used on a phrase, whole sentences may not create desired outcome.
 	 * @param toTurn
 	 * @param usePeriods
 	 * @return
 	 */
 	public static String getAcronym(String phrase, boolean usePeriods){
 		if (OddStringUtil.isEmpty(phrase)){
-			return "";
+			return EMPTY_STRING;
 		}
-		
 		StringBuilder re = new StringBuilder();
 		
-		String [] words = phrase.split(" ");
+		String [] words = phrase.split("[\\s]");
 		
 		for (String word: words){
-			if(OddStringUtil.skipForAcronym(word)){
+			if( OddStringUtil.skipForAcronym(word)){
 				continue;
 			} else {
 				if (OddStringUtil.isNumeric(word)){
@@ -33,7 +47,7 @@ public class OddStringUtil {
 				} else{
 					re.append(Character.toUpperCase(word.charAt(0)));
 					if (usePeriods){
-						re.append('.');	
+						re.append(PERIOD);	
 					}
 				}
 				
@@ -45,9 +59,11 @@ public class OddStringUtil {
 		
 	}
 	private static boolean skipForAcronym(String word){
-		if (word.equalsIgnoreCase("the") || word.equalsIgnoreCase("a") 
-				||word.equalsIgnoreCase("an")){
-			return true;
+		
+		for (String skipWord: acronymToSkip){
+			if (word.equalsIgnoreCase(skipWord)){
+				return true;
+			}
 		}
 		return false;
 	}
@@ -64,10 +80,19 @@ public class OddStringUtil {
 		}
 		return true;
 	}
-	
+	/**
+	 * Simply checks if the String is null or has 
+	 * no non-whitespace characters.
+	 * @param e
+	 * @return
+	 */
 	public static boolean isEmpty(String e){
 		return e == null || e.trim().length() == 0;
 	};
+	
+	public static String[] BreakUpLines(String string){
+		return string.split("[\\n\\t]");
+	}
 	public static String[] breakUpWords(String string){
 		return string.split(" ");
 	}
@@ -80,62 +105,165 @@ public class OddStringUtil {
 	 */
 	public static String get1337(String phrase){
 		StringBuilder re = new StringBuilder();
-		String [] words = OddStringUtil.breakUpWords(phrase);
 		
-		for (String word: words){
+		String[] lines = OddStringUtil.BreakUpLines(phrase);
+		String line;
+		int j = 0;
+		
+		if (lines.length > 0){
+			line = lines[j++];
+			re.append(OddStringUtil.get1337FromLine(line));
+		}
+		for (; j < lines.length; j++){
+			re.append("\n");
+			line = lines[j];
+			re.append(OddStringUtil.get1337FromLine(line));
+		}
+		
+		return re.toString();
+	}
+	private static String get1337FromLine(String line){
+		StringBuilder re = new StringBuilder();
+		String[] words = OddStringUtil.breakUpWords(line);
+		String word; 
+		int i = 0;
+		if (words.length > 0){
+			 word = words[i++];
 			if (isSpecial1337word(word)){
 				re.append(getSpecial1337word(word));
 			} else {
 				re.append(transformWordTo1337(word));
 			}
-			re.append(" ");
+			for (;i < words.length; i++){
+				re.append(" ");
+				 word = words[i];
+				if (isSpecial1337word(word)){
+					re.append(getSpecial1337word(word));
+				} else {
+					re.append(transformWordTo1337(word));
+				}
+				
+			}			
 		}
 		
 		return re.toString();
 	}
-	
+	/**
+	 * replaces individual letters in words with their
+	 * 1337 equivalents.
+	 * @param word
+	 * @return
+	 */
 	private static String transformWordTo1337(String word){
 		String returnWord = word;
 		
-		returnWord = returnWord.replace('e', '3');
-		returnWord = returnWord.replace('l', '1');
-		returnWord = returnWord.replace('o', '0');
+		for (int i = 0; i < replace1337Chars.length; i++){
+			returnWord = returnWord.replace(replace1337Chars[i], replace1337with[i]);
+		}
+		
 		return returnWord;
 	}
 	private static boolean isSpecial1337word(String word){
-		return special1337words.containsKey(word);
+		return special1337words.containsAnyKey(word);
 	}
 	private static String getSpecial1337word(String word){
-		return special1337words.get(word);
-	}
-	private static HashMap<String, String> get1337words(){
-		
-		HashMap<String, String> swords;
-		String[] args = {"elite", "1337", "the", "teh"};
-		
-		swords = getHashMap(args);
-		
-		
-		
-		return swords;
-	}
-	private static HashMap<String, String> getHashMap(String ...args){
-		HashMap<String, String> hashMap = new HashMap<String, String>();
-		if (args.length %2 != 0){
-			throw new IndexOutOfBoundsException();
-		}
-		
-		for (int i = 0; i < args.length; i+= 2){
-			hashMap.put(args[i], args[i+1]);
-		}
-		
-		return hashMap;
-	}
-	
-	public static void main(String[] args){
-		
-		String test = OddStringUtil.get1337("the elite cow jumped over the moon");
-		System.out.println(test);
+		return special1337words.getAny(word);
 	}
 
+	private static CaseInsensitiveMap getSpecial1337Map(){
+		CaseInsensitiveMap s1337Words = new CaseInsensitiveMap();
+		
+		String[] caseInsensitiveWords = {"elite", "1337", "the", "teh"};
+		s1337Words.put(caseInsensitiveWords, true);
+		
+		//adding LOL or else it would become 101
+		String[] caseSensitiveWords = {"THE", "TEH", "The", "Teh", "LOL", "LOL", "lol", "lol"};
+		s1337Words.put(caseSensitiveWords, false);
+		
+		
+		
+		
+		return s1337Words;
+	}
+	
+	private static class CaseInsensitiveMap extends HashMap<String, String>{
+		
+
+		private static final long serialVersionUID = 1L;
+
+		public String put(String key, String value){
+			return put(key.toLowerCase(), value, true);
+		}
+		
+		public String put(String key, String value, boolean caseInsensitive){
+			if (caseInsensitive){
+				return super.put(key.toLowerCase(), value);
+			} else {
+				return super.put(key, value);
+			}
+		}
+		public String get(String key, boolean caseInsensitive){
+			if (caseInsensitive){
+				return super.get(key.toLowerCase());
+			} else {
+				return super.get(key);
+			}
+		}
+		
+		/**
+		 * tries to find an exact match before trying
+		 * to find the caseInsensitive match.
+		 * @param key
+		 * @return
+		 */
+		public String getAny(String key){
+			String re = super.get(key);
+			
+			if (re == null){
+				re = super.get(key.toLowerCase());
+			}
+			return re;
+		}
+		
+		public void put(String[] keyValuePairs, boolean caseInsensitive){
+			if (keyValuePairs.length %2 != 0){
+				throw new IndexOutOfBoundsException();
+			}
+			
+			for (int i = 0; i < keyValuePairs.length; i+= 2){
+				put(keyValuePairs[i], keyValuePairs[i+1], caseInsensitive);
+			}
+		}
+		/**
+		 * tries to find the exact key match before
+		 * trying to find the casInsensitive match.
+		 * @param key
+		 * @return
+		 */
+		public boolean containsAnyKey(String key){
+			boolean found = super.containsKey(key);
+			
+			if (!found){
+				found = super.containsKey(key.toLowerCase());
+			}
+			
+			return found;
+			
+		}
+		public boolean containsKey(String key, boolean caseInsensitive){
+			if (caseInsensitive){
+				return super.containsKey(key.toLowerCase());
+			} else {
+				return super.containsKey(key);
+			}
+		}
+		public CaseInsensitiveMap(String[] keyValuePairs, boolean caseInsensitive){
+			super();
+			put(keyValuePairs, caseInsensitive);
+		}
+		
+		public CaseInsensitiveMap(){
+			super();
+		}
+	}
 }
